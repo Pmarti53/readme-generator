2d05a9557e46d2d8ce15d568aa40e5dd9cbe4951
peter <ptmartinezsound@gmail.com>
2021-04-09 15:50:54 -0500

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,31 @@
 // TODO: Include packages needed for this application
 
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of this project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+    inquirer
+    .prompt(questions)
+ };
 
 // Function call to initialize app
 init();
